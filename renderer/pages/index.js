864512085695de77b19166a76dd3ddc0b33d64ac
Mykhailo Aloshyn <mikhail.alyoshyn@gmail.com>
2024-09-07 23:58:46 +0300

    refactor:deleted debug header

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -1,12 +1,11 @@
 import MainScreen from "../components/MainScreen";
 const Home = () => {
 
-  return (
-    <div>
-      <h1>Hello Electron!</h1>
-        <MainScreen />
-    </div>
-  );
+    return (
+        <div>
+            <MainScreen />
+        </div>
+    );
 };
 
 export default Home;
