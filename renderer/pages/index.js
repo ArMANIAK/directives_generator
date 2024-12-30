2d96c54f3ca3cb633645b054a59af4bdfd09e361
Mykhailo Aloshyn <mikhail.alyoshyn@gmail.com>
2024-12-30 12:37:32 +0200

    feat:added page management

--- renderer/pages/index.js
+++ renderer/pages/index.js
@@ -1,14 +1,32 @@
-import MainScreen from "../windows/main-screen/MainScreen";
 import { Provider } from "react-redux"
-
 import { store } from "../store"
+
+import MainScreen from "../windows/main-screen/MainScreen";
 import DictionaryManagementScreen from "../windows/dictionary-management/DictionaryManagementScreen";
+import { useState } from "react";
+import { Button } from "@mui/material";
+
 const Home = () => {
 
+    const [ currentPage, setCurrentPage ] = useState("main")
     return (
         <Provider store={store}>
-            {/*<MainScreen />*/}
-            <DictionaryManagementScreen />
+            <Button
+                style={{padding: "15px", backgroundColor: "green", borderRadius: "0"}}
+                variant="contained"
+                onClick={() => setCurrentPage("main")}
+            >
+                Наказ командира по стройовій
+            </Button>
+            <Button
+                style={{padding: "15px", backgroundColor: "blue", borderRadius: "0"}}
+                variant="contained"
+                onClick={() => setCurrentPage("admin")}
+            >
+                Редагувати посади/підрозділи/персонал
+            </Button>
+            { currentPage === "main" && <MainScreen /> }
+            { currentPage === "admin" && <DictionaryManagementScreen /> }
         </Provider>
     );
 };
